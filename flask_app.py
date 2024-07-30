bd057baecb7d3e606c842e5b29f010224d9fcdfb
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2024-07-30 17:51:49 +0700

    minor change + new question_answer list

--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import random
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # replace with your secret key
+file_name = "question_answer_humn110.csv"
 
 # Read questions from CSV file
 questions = []
-with open('question_answer.csv', 'r') as file:
+with open(file_name, 'r') as file:
     reader = csv.reader(file, delimiter='|')
     next(reader)
     for row in reader:
@@ -23,23 +23,26 @@ def next_question():
     q, a = list(random.choice(questions).items())[0]
     return render_template('quiz.html', question=q, answer=a)
 
-# @app.route('/')
-# def quiz():
-#     session['question_index'] = 0
-#     q, a = list(questions[session['question_index']].items())[0]
-#     return render_template('quiz.html', question=q, answer=a)
+# If you want to end it.
+"""
+@app.route('/')
+def quiz():
+    session['question_index'] = 0
+    q, a = list(questions[session['question_index']].items())[0]
+    return render_template('quiz.html', question=q, answer=a)
 
-# @app.route('/next')
-# def next_question():
-#     session['question_index'] += 1
-#     if session['question_index'] >= len(questions):
-#         return redirect(url_for('end'))
-#     q, a = list(questions[session['question_index']].items())[0]
-#     return render_template('quiz.html', question=q, answer=a)
+@app.route('/next')
+def next_question():
+    session['question_index'] += 1
+    if session['question_index'] >= len(questions):
+        return redirect(url_for('end'))
+    q, a = list(questions[session['question_index']].items())[0]
+    return render_template('quiz.html', question=q, answer=a)
 
-# @app.route('/end')
-# def end():
-#     return "End of questions."
+@app.route('/end')
+def end():
+    return "End of questions."
+"""
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="192.168.1.19", port=5000)
